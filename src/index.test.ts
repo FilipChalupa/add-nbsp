7f8fd87c5562441bc1723457b6a92876684fb142
Artem Kamysh <a.kamysh@corp.mail.ru>
2022-06-12 05:59:25 +0300

    1.0.5

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,17 @@
 import { autoNbsp } from './index';
 
 test('With no nbsp', () => {
-  expect(autoNbsp('Common description')).toBe('Common description');
+	expect(autoNbsp('Common description')).toBe('Common description');
 });
 
 test('With one nbsp', () => {
-  expect(autoNbsp('What are they doing?')).toBe('What&nbsp;are they doing?');
+	expect(autoNbsp('What are they doing?')).toBe('What&nbsp;are they doing?');
 });
 
 test('With no nbsp and custom options minCharInWord = 2', () => {
-  expect(autoNbsp('What are they doing?', { minCharInWord: 2 })).toBe('What are they doing?');
+	expect(autoNbsp('What are they doing?', { minCharInWord: 2 })).toBe('What are they doing?');
 });
 
 test('With two nbsp and custom options minCharInWord = 5', () => {
-  expect(autoNbsp('What are they doing?', { minCharInWord: 5 })).toBe('What&nbsp;are&nbsp;they doing?');
+	expect(autoNbsp('What are they doing?', { minCharInWord: 5 })).toBe('What&nbsp;are&nbsp;they doing?');
 });
